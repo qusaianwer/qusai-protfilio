d460f561fd0a11227ddb9ab590b6ebb12a943e33
QUSAI ANWER <qusaianwer06@gmail.com>
2026-09-25 20:27:17 +0300

    fix image

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
+import logo from "../photo/photo_2026-09-23_21-24-46.jpg";
+
 function Navbar(){
     return(
-        <nav className="navbar">
-            <div className="logo-group">
-            <img src="src/photo/photo_2026-09-23_21-24-46.jpg" alt="" className="logo-img" />            
-            <h2 className="logo">Qusai</h2>
-            </div>
-            <ul className="nav-links">
-                <li>Home</li>
-                <li>About</li>
-                <li>My project</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+      <nav className="navbar">
+        <div className="logo-group">
+        <img src={logo} alt="" className="logo-img" />
+        <h2 className="logo">Qusai</h2>
+        </div>
+        <ul className="nav-links">
+            <li>Home</li>
+            <li>About</li>
+            <li>My project</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
     );
 }
 export default Navbar;
