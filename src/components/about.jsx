9745060459b1d1c8cb5ae9d97ef02fa179cdd336
QUSAI ANWER <qusaianwer06@gmail.com>
2026-09-26 04:02:05 +0300

    add about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,10 +3,13 @@ function About() {
     <section className="about" id="about">
       <h2>About</h2>
       <p>
-       I'm Qusay, a web developer passionate about creating simple and effective digital experiences.
+       I’m Qusai Anwer, a Software Engineering student at International Islamic University of Jordan (IIU).
 
-I love learning new technologies and developing projects that solve real-world problems.
-      </p>
+I’m passionate about software development, problem solving, and building real-world projects that turn ideas into useful digital experiences.
+
+Currently, I’m developing my skills in Java, Python, C++, Web Development, and Automation, while continuously learning new technologies and improving my programming fundamentals.
+
+My goal is to grow into a skilled software engineer by building meaningful projects, solving challenging problems, and gaining practical experience along the way.</p>
     </section>
   );
 }
